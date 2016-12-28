7d07b1fc630e96843a466c21aa5a40b9fe24afc7
care <care@caredeimac-3.local>
2016-12-28 09:38:07 +0800

    Initial Commit

---
+++ LuochuanAD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LuochuanAD
+//
+//  Created by care on 16/12/28.
+//  Copyright © 2016年 luochuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LuochuanAD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LuochuanAD
+//
+//  Created by care on 16/12/28.
+//  Copyright © 2016年 luochuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
