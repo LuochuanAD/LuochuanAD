20dd497f377f66d242d8991407edad2bab5ed49a
care <care@caredeimac-3.local>
2017-09-11 17:09:11 +0800

    高吞吐量且线程安全

--- LuochuanAD/HPUpdaterService.h
+++ LuochuanAD/HPUpdaterService.h
@@ -9,5 +9,14 @@
 #import <Foundation/Foundation.h>
 #import "HPUser.h"
 @interface HPUpdaterService : NSObject
++ (HPUpdaterService *)sharedInstance;
+
+//读写锁 (线程安全且高吞吐量)
+- (id)objectForKey:(id<NSCopying>)key;
+- (void)setObject:(id)object forKey:(id<NSCopying>)key;
+- (id)removeObjectForKey:(id<NSCopying>)key;
+- (void)clear;
+
+//同步块
 - (void)updateUser:(HPUser *)user properties:(NSDictionary *)properties;
 @end
--- LuochuanAD/HPUpdaterService.m
+++ LuochuanAD/HPUpdaterService.m
@@ -7,8 +7,60 @@
 //
 
 #import "HPUpdaterService.h"
+static const  char * const kCatchQueueName="com.luochuanad.luochuanad";
+
+
+@interface HPUpdaterService()
+@property (nonatomic, readonly) NSMutableDictionary *catchObjects;
+@property (nonatomic, readonly) dispatch_queue_t queue;
+@end
 
 @implementation HPUpdaterService
++ (HPUpdaterService *)sharedInstance{
+    static HPUpdaterService *instance=nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance=[[HPUpdaterService alloc]init];
+    });
+    return instance;
+}
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _catchObjects=[NSMutableDictionary dictionary];
+        _queue=dispatch_queue_create(kCatchQueueName, DISPATCH_QUEUE_PRIORITY_DEFAULT);
+    }
+    return self;
+}
+- (id)objectForKey:(id<NSCopying>)key{
+    __block id rv=nil;
+    dispatch_async(self.queue, ^{
+        rv=[self.catchObjects objectForKey:key];
+    });
+    return rv;
+}
+- (void)setObject:(id)object forKey:(id<NSCopying>)key{
+    dispatch_barrier_async(self.queue, ^{
+        [self.catchObjects setObject:object forKey:key];
+    });
+}
+- (id)removeObjectForKey:(id<NSCopying>)key{
+    __block id rv=nil;
+    dispatch_async(self.queue, ^{
+        rv=[self.catchObjects objectForKey:key];
+        [self.catchObjects removeObjectForKey:key];
+    });
+    return rv;
+}
+- (void)clear{
+    dispatch_async(self.queue, ^{
+        [self.catchObjects removeAllObjects];
+    });
+}
+
+
+
 - (void)updateUser:(HPUser *)user properties:(NSDictionary *)properties{
     @synchronized (user) {
         NSString *fn=[properties objectForKey:@"firstName"];
